63820b370c5c0cbe6f46f29f5f4dfc08ad5c35c8
Ji San <dbwltks@gmail.com>
2022-08-10 00:27:11 +0900

    Vue3-Profile start

--- blog/src/App.vue
+++ blog/src/App.vue
@@ -1,13 +1,17 @@
 <template>
   <NavBar />
+  <Profile />
 </template>
 
 <script>
-import NavBar from './components/NavBar.vue'
+import NavBar from '/@components/NavBar.vue'
+import Profile from '/@components/Profile.vue'
+
 export default {
   name: 'App',
   components: {
     NavBar,
+    Profile,
   },
 }
 </script>
